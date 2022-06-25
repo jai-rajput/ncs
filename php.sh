ff287453238ae382a5352f2611efa6f30667c8d9
jai-rajput <devjairajput22@gmail.com>
2022-06-25 13:40:49 +0530

    php.sh is succesfuly commited

---
+++ php.sh
@@ -0,0 +1,12 @@
+echo "which php version do you want to install {5.6,7.0,7.1,7.2,7.3,7.4,8.0}:"
+read PHP_VER;
+if [ "$PHP_VER" == "5.6" ] || [ "$PHP_VER" == "7.0" ] || [ "$PHP_VER" == "7.4" ] || [ "$PHP_VER" == "7.3" ] || [ "$PHP_VER" == "7.2" ] || [ "$PHP_VER" == "7.1" ] || [ "$PHP_VER" == "8.0" ]; 
+then
+    sudo add-apt-repository ppa:ondrej/php
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt install php$PHP_VER
+    echo "PHP version $PHP_VER successfully installed!"
+else
+    echo "version $PHP_VER is not available"
+fi
